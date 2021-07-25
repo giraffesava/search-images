da11cc32991fcedf3d9774c8307eb01cc0465606
Sava <savely54@mail.ru>
2021-07-26 05:17:13 +0700

    fixed

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import './Button.scss'
 
 interface Props {
+  active?: boolean
   type: string
   children: string
   onClick: () => void
 }
 
-const Button: React.FC<Props> = ({ type, children, onClick }) => {
+const Button: React.FC<Props> = ({ type, children, onClick, active }) => {
   return (
-    <button className={[type, 'btn'].join(' ')} onClick={onClick}>
+    <button
+      className={[type, 'btn'].join(' ')}
+      disabled={active}
+      onClick={onClick}
+    >
       {children}
     </button>
   )
