3944aecb34c0f48b2222e14d042b98b934d69dfa
Sava <savely54@mail.ru>
2021-07-25 21:40:40 +0700

    added store

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
+import { Provider } from 'react-redux'
 import App from './components/App/App'
+import { initStore } from './store/store'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = initStore()
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+)
