332eb346627c281f80f1f412d0c13847d8ec09b4
Sava <savely54@mail.ru>
2021-07-27 18:21:49 +0700

    added types

--- src/store/types.tsx
+++ src/store/types.tsx
@@ -2,16 +2,22 @@ export enum Pictures {
   GET_PICTURES = 'GET_PICTURES',
   SUCCESS_GETTING_PICTURES = 'SUCCESS_GETTING_PICTURES',
   FAILED_GETTING_PICTURES = 'SUCCESS_FAILED_PICTURES',
+  DELETE_DATA_PICTURES = 'DELETE_DATA_PICTURES',
+  FAILED_SERVER = 'FAILED_SERVER',
+  POLLING_START = 'POLLING_START',
+  POLLING_END = 'POLLING_END',
 }
 
 export interface PictureTypes {
   type: Pictures
   keyword?: string
-  data?: []
+  data?: any[]
 }
 
 export interface PictureStore {
   loading: boolean
-  error: boolean
-  data: []
+  serverError: boolean
+  data: any[]
+  notFoundError: boolean
+  polling: boolean
 }
