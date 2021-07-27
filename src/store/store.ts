c036ef997673ecb62d9b8284bde0e736e96860d4
Sava <savely54@mail.ru>
2021-07-28 05:46:55 +0700

    added watchers

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,10 @@
 import { createStore, applyMiddleware } from 'redux'
-import pictureReducer from './pictures/pictures.reducer'
+import pictureReducer from './pictures_redux/reducers/pictures.reducer'
 import createSagaMiddleware from 'redux-saga'
 import { composeWithDevTools } from 'redux-devtools-extension'
-import { getPicturesWatcher } from './pictures/pictures.saga'
+import { getMultiPicturesWatcher } from './pictures_redux/sagas/pictures.multi.saga'
+import { getSinglePicturesWatcher } from './pictures_redux/sagas/pictures.single.saga'
+import { getPollingPicturesWatcher } from './pictures_redux/sagas/pictures.polling.saga'
 
 export const initStore = (): any => {
   const sagaMiddleware = createSagaMiddleware()
@@ -10,6 +12,9 @@ export const initStore = (): any => {
     pictureReducer,
     composeWithDevTools(applyMiddleware(sagaMiddleware)),
   )
-  sagaMiddleware.run(getPicturesWatcher)
+  sagaMiddleware.run(getMultiPicturesWatcher)
+  sagaMiddleware.run(getSinglePicturesWatcher)
+  sagaMiddleware.run(getPollingPicturesWatcher)
+
   return store
 }
