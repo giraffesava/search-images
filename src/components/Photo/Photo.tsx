f8043811846ced36755cf6c89ced87baacee6188
Sava <savely54@mail.ru>
2021-07-27 18:21:34 +0700

    Created a component

--- src/components/Photo/Photo.tsx
+++ src/components/Photo/Photo.tsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import './Photo.scss'
-
+import { v4 as uuidv4 } from 'uuid'
 interface Props {
-  url: string
-  title: string
+  url: any
+  title: any
 }
 
 const Photo: React.FC<Props> = ({ url, title }) => {
-  return <img src={url} alt={title} />
+  return (
+    <div className="hoho">
+      {Array.isArray(url) ? (
+        <div className="double-image">
+          {url.map((urls) => (
+            <img src={urls} alt={title} className="images" key={uuidv4()} />
+          ))}
+        </div>
+      ) : (
+        <img src={url} alt={title} className="image" />
+      )}
+    </div>
+  )
 }
 
 export default Photo
