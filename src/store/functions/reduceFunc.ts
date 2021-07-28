7306f097c97d0b61ce2f34d17ade0fe4f277f953
Sava <savely54@mail.ru>
2021-07-28 15:30:51 +0700

    deleted console.log

--- src/store/functions/reduceFunc.ts
+++ src/store/functions/reduceFunc.ts
@@ -1,6 +1,5 @@
 // форматирование данных, переход из массива в объект
 const reduceFunc = (data: any[]) => {
-  console.log(data)
   return data.reduce((acc, item) => {
     if (data.length > 1) {
       for (let key in item) {
